e3ddc7d1356c587b4cd0e6786ce4cb572c407713
VidhanSomkuwar <139569762+VidhanSomkuwar@users.noreply.github.com>
2024-09-20 14:37:33 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // This tells Next.js to export the site as static HTML
+    // basePath: '/TodoList', // Replace with the name of your GitHub repository
+    // assetPrefix: '/TodoList/', // Same as the basePath
+  };
+  
+  export default nextConfig;
+  
